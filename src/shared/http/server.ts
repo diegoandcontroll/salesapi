d79bc13e9a1aa269d021eb2efea233e5b66da453
diegoandcontroll <lukasxdp2@gmail.com>
2022-03-25 02:29:57 -0300

    feat: create migration table products

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,6 +1,9 @@
-import express from 'express';
+import 'reflect-metadata';
+import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 const app = express();
 
 app.use(cors());
@@ -8,6 +11,21 @@ app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(
+  (error: Error, request: Request, response: Response, next: NextFunction) => {
+    if (error instanceof AppError) {
+      return response.status(error.statusCode).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+    return response.status(500).json({
+      status: 'error',
+      message: 'Internal Server Error',
+    });
+  },
+);
 
 app.listen(3000, () => {
   console.log('server start');
