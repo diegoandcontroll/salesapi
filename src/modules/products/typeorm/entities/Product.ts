ef1c26c4969f9bc3cba9007050a91cd91e1a27f1
diegoandcontroll <lukasxdp2@gmail.com>
2022-03-25 09:34:15 -0300

    feat: create entity and repository products

---
+++ src/modules/products/typeorm/entities/Product.ts
@@ -0,0 +1,31 @@
+/* eslint-disable prettier/prettier */
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('products')
+class Product {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column('decimal')
+  price: number;
+
+  @Column('int')
+  quantity: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+
+export default Product;
---
+++ src/modules/products/typeorm/repositories/ProductsRepositorie.ts
@@ -0,0 +1,15 @@
+/* eslint-disable prettier/prettier */
+import { EntityRepository, Repository } from 'typeorm';
+
+import Product from '../entities/Product';
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = this.findOne({
+      where: name,
+    });
+
+    return product;
+  }
+}
