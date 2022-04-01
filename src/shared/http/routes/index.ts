10536091c6a8d52dd9ff48bf7849d34dd5076050
diegoandcontroll <lukasxdp2@gmail.com>
2022-04-01 02:37:50 -0300

    fix: create controller and routes products

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,8 +1,6 @@
+import productsRouter from '@modules/products/routes/products.routes';
 import { Router } from 'express';
 
 const routes = Router();
-
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hellow' });
-});
+routes.use('/products', productsRouter);
 export default routes;
